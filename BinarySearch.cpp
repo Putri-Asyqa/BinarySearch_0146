921c340c132d981ae3263e9317bc2d9d24e94d1a
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-04-16 10:50:16 +0700

    menambahkan prosedur display

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -47,5 +47,23 @@ void bubbleSortArray()
                 element[j + 1] = temp;
             }
         }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
+
+void display()
+{
+    cout << "\n===================================\n";
+    cout << "  Elemen Array Setelah Diurutkan (Asc\n)";
+    cout << "=====================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
     }
+    cout << endl;
 }
